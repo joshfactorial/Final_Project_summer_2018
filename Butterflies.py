6165ab5c7ce7c31afc9ae358e9506807af234976
joshfactorial <joshfactorial@gmail.com>
2018-07-31 08:59:10 -0500

    fleshing out the butterfly class

--- Butterflies.py
+++ Butterflies.py
@@ -11,16 +11,16 @@ class Field(DataFrame):
         DataFrame.__init__(self, *args, **kwargs)
         for column in self:
             for row in self:
-                if self[column][row] not in (1, 2, 3, 'C', 'F', 'S'):
-                    raise ValueError("values must be either 1, 2, 3, C, F, or S")
+                if self[column][row] not in (1, 2, 3):
+                    raise ValueError("values must be either 1 (crop), 2 (food), or 3 (shelter)")
         try:
             for column in self:
-                self[column] = self[column].replace(1, 'C')
-                self[column] = self[column].replace(2, 'F')
-                self[column] = self[column].replace(3, 'S')
+                self[column] = self[column].replace(1, '=')
+                self[column] = self[column].replace(2, 'o')
+                self[column] = self[column].replace(3, '*')
 
         except ValueError:
-            print("values must be either 1, 2, 3, C, F, or S")
+            print("values must be either 1 (crop), 2 (food), or 3 (shelter)")
 
     @classmethod
     def random_field(cls, length, width, percent_crops, percent_food, percent_shelter):
@@ -35,19 +35,20 @@ class Field(DataFrame):
         # there shouldn't be more than three cells difference because of how I rounded it, so this shouldn't
         # affect the final result much
         distro = area - (number_crop_cells + number_food_cells + number_shelter_cells)
-        # I'll prioritize adding food cells if they elected to have them, or else I'll add shelter cells
+        # I'll prioritize adding shelter cells if they elected to have them, or else I'll add food cells
+        # I feel like most farmers would prioritize wind breaks over feeding butterflies
         while distro > 0:
-            if number_food_cells > 0:
-                number_food_cells += 1
-            else:
-                number_shelter_cells += 1
-            distro = area - (number_crop_cells + number_food_cells + number_shelter_cells)
-        # If there are any shelter cells, remove those first, then food cells
-        while distro < 0:
             if number_shelter_cells > 0:
-                number_shelter_cells -= 1
+                number_shelter_cells += 1
             else:
+                number_food_cells += 1
+            distro = area - (number_crop_cells + number_food_cells + number_shelter_cells)
+        # If there are any food cells, remove those first, then shelter cells
+        while distro < 0:
+            if number_food_cells > 0:
                 number_food_cells -= 1
+            else:
+                number_shelter_cells -= 1
             distro = area - (number_crop_cells + number_food_cells + number_shelter_cells)
         try:
             distro == 0
@@ -69,21 +70,33 @@ class Field(DataFrame):
                     number_food_cells -= 1
         # reality check
         print(random_f.stack().value_counts())
-        return Field(random_f)
+        return cls(random_f)
 
 
 class Butterfly:
-    def __init__(self):
+    def __init__(self, length, width):
         self.food_level = np.random.randint(0, 100)
+        self.length = length
+        self.width = width
+        variable = np.random.choice([0, 1, length-1, width-1])
+        if variable == 1:
+            self.position = (0, np.random.randint(0, length))
+        elif variable == 0:
+            self.position = (np.random.randint(0, width), 0)
+        elif variable == length-1:
+            self.position = (np.random.randint(0, width), variable)
+        else:
+            self.position = (variable, np.random.randint(0, length))
 
     def get_food_level(self):
         return str(self.food_level)
+
+    def get_position(self):
+        return "{}, {}".format(self.position[0], self.position[1])
 
     def __str__(self):
         return 'Monarch butterfly with {}% food'.format(self.food_level)
 
 
 if __name__ == '__main__':
-    f = Field.random_field(5, 5, 85, 14.9, 0.1)
-    print(f)
 
