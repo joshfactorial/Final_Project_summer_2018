7f96dd7290cd60b6da909b999e9617a58ae89cce
joshfactorial <joshfactorial@gmail.com>
2018-08-01 02:54:10 -0500

    working more on the butterfly class and related functions

--- Butterflies.py
+++ Butterflies.py
@@ -5,7 +5,30 @@ import math
 
 
 class Field(DataFrame):
-    """Create a field with sections denote C for crop, F for food, or S for shelter"""
+    """
+    Create a field with sections denote C for crop, F for food, or S for shelter
+
+    >>> f = Field([[1, 2, 2, 1], [1, 2, 2, 1], [1, 2, 2, 1], [1, 2, 2, 1], [1, 2, 2, 1]])
+    >>> print(f)
+       0  1  2  3
+    0  =  o  o  =
+    1  =  o  o  =
+    2  =  o  o  =
+    3  =  o  o  =
+    4  =  o  o  =
+    >>> f2 = Field([[1, 1, 1, 2], [1, 1, 1, 2], [1, 1, 1, 2], [1, 1, 1, 4]])
+    Traceback (most recent call last):
+      File "C:/Users/Joshua/PycharmProjects/Final_Project/Butterflies.py", line 117, in <module>
+        f = Field([[1, 1, 1, 2], [1, 1, 1, 2], [1, 1, 1, 2], [1, 1, 1, 4]])
+      File "C:/Users/Joshua/PycharmProjects/Final_Project/Butterflies.py", line 30, in __init__
+        raise ValueError("values must be either 1 (crop), 2 (food), or 3 (shelter)")
+    ValueError: values must be either 1 (crop), 2 (food), or 3 (shelter)
+    >>> rf = Field.random_field(10, 10, 85, 10, 5)
+    1    85
+    2    10
+    3     5
+    dtype: int64
+    """
 
     def __init__(self, *args, **kwargs):
         DataFrame.__init__(self, *args, **kwargs)
@@ -74,8 +97,12 @@ class Field(DataFrame):
 
 
 class Butterfly:
+    """
+
+    """
     def __init__(self, length, width):
-        self.food_level = np.random.randint(0, 100)
+        self.food_level = float(np.random.randint(0, 101))
+        self.status = "alive"
         self.length = length
         self.width = width
         variable = np.random.choice([0, 1, length-1, width-1])
@@ -88,15 +115,193 @@ class Butterfly:
         else:
             self.position = (variable, np.random.randint(0, length))
 
+    def get_status(self):
+        return self.status
+
     def get_food_level(self):
         return str(self.food_level)
 
     def get_position(self):
-        return "{}, {}".format(self.position[0], self.position[1])
+        return list(self.position)
 
     def __str__(self):
         return 'Monarch butterfly with {}% food'.format(self.food_level)
 
+    def move(self):
+        if self.food_level > 50.0:
+            # if it's belly is full, move with a preference toward North
+            roll_die = np.random.random_sample()
+            if roll_die <= 0.75:
+                # Northly
+                if self.position[1] - 1 > 0:
+                    second_die = np.random.random_sample()
+                    if second_die > 0.05:
+                        self.position = (self.position[0], self.position[1] - 1)
+                    else:
+                        self.status = 'dead'
+                else:
+                    second_die = np.random.random_sample()
+                    if second_die > 0.05:
+                        self.status = "exit"
+                    elif second_die <= 0.05 and second_die >0.01:
+                        self.status = "dead"
+                    else:
+                        pass
+            elif 0.75 < roll_die and roll_die <=0.83:
+                # Easterly
+                second_die = np.random.random_sample()
+                if second_die >= 0.01:
+                    self.position = (self.position[0] + 1, self.position[1])
+                else:
+                    self.status = "dead"
+            elif roll_die > 0.83 and roll_die <= 0.91:
+                # Southerly
+                second_die = np.random.random_sample()
+                if second_die >= 0.01:
+                    self.position = (self.position[0], self.position[1] + 1)
+                else:
+                    self.status = "dead"
+            elif roll_die > 0.91 and roll_die <= 0.99:
+                # Westerly
+                second_die = np.random.random_sample()
+                if second_die >= 0.01:
+                    self.position = (self.position[0] - 1, self.position[1])
+                else:
+                    self.status = 'dead'
+            else:
+                # stay
+                second_die = np.random.random_sample()
+                if second_die >= 0.10:
+                    pass
+                else:
+                    self.status = 'dead'
+        elif self.food_level > 25.0 and self.food_level <= 50.0:
+            roll_die = np.random.random_sample()
+            if roll_die <= 0.05:
+                self.status = 'dead'
+            else:
+                seek_food(self)
+        else:
+            roll_die = np.random.random_sample()
+            if roll_die < 0.1:
+                self.status = 'dead'
+            else:
+                seek_food(self)
+        self.food_level -= 0.0225
+
+
+def seek_food(monarch: Butterfly) -> Butterfly:
+    location = monarch.get_position()
+    return location
+    # TODO figure out a way to find the nearest food source and have the monarch move there with high probability
+
+
+def create_standard_test(iterations: int) -> Field:
+    # fields are assumed to have a scale 1 cell has dimension 15 meters x 15 meters
+    # create the top and bottom rows of the field
+    base_field_base_rows = [2] * 100
+    base_field_base_rows[0] = 3
+    base_field_base_rows[99] = 3
+    # create the standard middle row
+    base_field_middle_rows = [1] * 100
+    base_field_middle_rows[0] = 3
+    base_field_middle_rows[99] = 3
+    # Build up a single standard field
+    standard_field = [base_field_base_rows]
+    for i in range(0, 98):
+        standard_field.append(base_field_middle_rows)
+    standard_field.append(base_field_base_rows)
+    # create the standard test site
+    test_site = []
+    for i in range(0, iterations+1):
+        test_site += standard_field
+    return Field(pd.DataFrame(test_site))
+
+
+def create_food_heavy_test(iterations: int) -> Field:
+    # fields are assumed to have a scale 1 cell has dimension 15 meters x 15 meters
+    # create the top and bottom rows of the field
+    base_field_base_rows = [2] * 100
+    # create the standard middle row
+    base_field_middle_rows = [1] * 100
+    base_field_middle_rows[0] = 2
+    base_field_middle_rows[99] = 2
+    # Build up a single standard field
+    standard_field = [base_field_base_rows]
+    for i in range(0, 98):
+        standard_field.append(base_field_middle_rows)
+    standard_field.append(base_field_base_rows)
+    # create the standard test site
+    test_site = []
+    for i in range(0, iterations+1):
+        test_site += standard_field
+    return Field(pd.DataFrame(test_site))
+
+
+def create_shelter_heavy_test(iterations: int) -> Field:
+    # fields are assumed to have a scale 1 cell has dimension 15 meters x 15 meters
+    # create the top and bottom rows of the field
+    base_field_base_rows = [3] * 100
+    base_field_base_rows[0] = 3
+    base_field_base_rows[99] = 3
+    # create the standard middle row
+    base_field_middle_rows = [1] * 100
+    base_field_middle_rows[0] = 2
+    base_field_middle_rows[99] = 2
+    # Build up a single standard field
+    standard_field = [base_field_base_rows]
+    for i in range(0, 98):
+        standard_field.append(base_field_middle_rows)
+    standard_field.append(base_field_base_rows)
+    # create the standard test site
+    test_site = []
+    for i in range(0, iterations+1):
+        test_site += standard_field
+    return Field(pd.DataFrame(test_site))
+
+
+def create_middle_food_windbreak_test(iterations: int) -> Field:
+    # fields are assumed to have a scale 1 cell has dimension 15 meters x 15 meters
+    # create the top and bottom rows of the field
+    base_field_base_rows = [3] * 100
+    # create the standard middle row
+    base_field_middle_rows = [1] * 100
+    base_field_middle_rows[0] = 3
+    base_field_middle_rows[99] = 3
+    base_field_middle_rows[49] = 2
+    # Build up a single standard field
+    standard_field = [base_field_base_rows]
+    for i in range(0, 98):
+        standard_field.append(base_field_middle_rows)
+    standard_field.append(base_field_base_rows)
+    # create the standard test site
+    test_site = []
+    for i in range(0, iterations+1):
+        test_site += standard_field
+    return Field(pd.DataFrame(test_site))
+
+
+def create_food_middle_windbreak_test(iterations: int) -> Field:
+    # fields are assumed to have a scale 1 cell has dimension 15 meters x 15 meters
+    # create the top and bottom rows of the field
+    base_field_base_rows = [2] * 100
+    # create the standard middle row
+    base_field_middle_rows = [1] * 100
+    base_field_middle_rows[0] = 2
+    base_field_middle_rows[99] = 2
+    base_field_middle_rows[49] = 3
+    # Build up a single standard field
+    standard_field = [base_field_base_rows]
+    for i in range(0, 98):
+        standard_field.append(base_field_middle_rows)
+    standard_field.append(base_field_base_rows)
+    # create the standard test site
+    test_site = []
+    for i in range(0, iterations+1):
+        test_site += standard_field
+    return Field(pd.DataFrame(test_site))
+
 
 if __name__ == '__main__':
-
+    b1 = Butterfly(100, 100)
+    print(b1)
